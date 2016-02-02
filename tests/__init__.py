5a0cf3f190030797acef0b1a5300a05bedadbb38
alexandermendes <alexanderhmendes@gmail.com>
2016-02-02 16:48:22 +0000

    Add tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,5 +13,7 @@ def setUpPackage():
     """Setup the plugin."""
     from default import flask_app
     with flask_app.app_context():
+        settings = os.path.abspath('./settings_test.py')
+        flask_app.config.from_pyfile(settings)
         plugin_dir = os.path.dirname(plugin.__file__)
         plugin.PyBossaDiscourse(plugin_dir).setup()
