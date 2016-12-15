e2c9d5a9f71864ce4dc9701a2cea3fcff9bf633b
alexandermendes <alexanderhmendes@gmail.com>
2016-12-15 19:11:51 +0000

    Add test

--- tests/test_client.py
+++ tests/test_client.py
@@ -182,3 +182,15 @@ class TestClient(Test):
         self.client.search('something')
 
         assert mock_request.called_with('GET', url, params)
+
+
+    @patch('pybossa_discourse.client.Client.create_user', new=mock_create)
+    @patch('pybossa_discourse.client.current_user', new=mock_user)
+    @patch('pybossa_discourse.client.requests.request', new=mock_request)
+    def test_get_username_returns_none_when_create_user_fails(self):
+        endpoint = '/admin/users/list/all.json'
+        url = '{0}{1}'.format(self.url, endpoint)
+        params = {'filter': 'me@me.com'}
+        self.client._get_username('something')
+
+        assert mock_request.called_with('GET', url, params)
