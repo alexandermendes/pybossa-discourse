dfefa214f1affac167fac6aaf139012fb9fc3d9e
alexandermendes <alexanderhmendes@gmail.com>
2016-12-16 05:01:57 +0000

    Use relative imports

--- pybossa_discourse/extensions.py
+++ pybossa_discourse/extensions.py
@@ -1,8 +1,8 @@
 # -*- coding: utf8 -*-
 """Extensions module for pybossa-discourse."""
 
-from pybossa_discourse.client import DiscourseClient
-from pybossa_discourse.sso import DiscourseSSO
+from .client import DiscourseClient
+from .sso import DiscourseSSO
 
 __all__ = ['discourse_client', 'discourse_sso']
 
